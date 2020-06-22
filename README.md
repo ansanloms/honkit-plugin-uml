0e400aa46d2efe71bb0cd5233f0eb5fce9431db9
ansanloms <ansanloms@users.noreply.github.com>
2020-06-23 06:18:46 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# honkit-plugin-uml
+
+[PlantUML](https://plantuml.com/) Plugin for [HonKit](https://github.com/HonKit/HonKit).
+
+## Install
+
+```
+npm install git+https://github.com/ansanloms/honkit-plugin-uml.git
+```
+
+## Usage
+
+Add the plugin to `book.json` .
+
+```json
+{
+  "plugins": ["uml"]
+}
+```
+
+## Options
+
+```json
+"pluginsConfig": {
+  "uml": {
+     "format": "svg",
+     "charset": "utf8",
+     "config": "classic"
+  }
+}
+```
+
+- `format`
+	+ Format of the generated images. `png` , `svg` , `ascii` .
+- `charset`
+	+ Use a specific charset.
+- `config`
+	+ design settings. `classic` , `monochrome` or file path.
+
---
+++ index.js
@@ -0,0 +1,55 @@
+const plantuml = require("node-plantuml");
+
+const defaultConfig = {
+  format: "png",
+  charset: "utf8",
+  config: "classic",
+};
+
+// https://stackoverflow.com/questions/5251520/how-do-i-escape-some-html-in-javascript/5251551
+async function replaceAsync(str, regex, asyncFn) {
+  const promises = [];
+  str.replace(regex, (match, ...args) => {
+    const promise = asyncFn(match, ...args);
+    promises.push(promise);
+  });
+  const data = await Promise.all(promises);
+  return str.replace(regex, () => data.shift());
+}
+
+module.exports = {
+  hooks: {
+    "page:before": async function(page) {
+      const config = defaultConfig;
+      Object.assign(config, this.config.get("pluginsConfig.uml", {}));
+
+      page.content = await replaceAsync(page.content, /```plantuml((.*[\r\n]+)+?)?```/igm, async (match, uml) => {
+        const gen = plantuml.generate(uml, {
+          format: config.format,
+          charset: config.charset,
+          config: config.config
+        });
+
+        const chunks = [];
+        for await (const chunk of gen.out) {
+          chunks.push(chunk);
+        }
+
+        const buffer = Buffer.concat(chunks);
+
+        switch (config.format) {
+          case "ascii":
+            return `<pre>${buffer.toString(config.charset)}</pre>`;
+
+          case "svg":
+            return `<img src="data:image/svg+xml;base64,${buffer.toString("base64")}">`;
+
+          case "png":
+            return `<img src="data:image/png;base64,${buffer.toString("base64")}">`;
+        }
+      });
+
+      return page;
+    }
+  },
+};
